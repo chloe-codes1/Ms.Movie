bdf3b03ee2444270caf38f1b5141a862c3d73d18
오지수 <wltn0213@gmail.com>
2020-06-11 17:10:04 +0900

    accounts & init_setting

--- backend/django_server/urls.py
+++ backend/django_server/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reviews/', include('reviews.urls')),
+    path('rest-auth/', include('rest-auth.urls')),
+    path('rest-auth/signup/', include('rest_auth.registration.urls')),
 ]
