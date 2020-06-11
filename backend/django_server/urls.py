821123316872d6597cde5bec883021e73ffb95c8
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-06-11 20:06:49 +0900

    Add movie model & movie data scraping logic

--- backend/django_server/urls.py
+++ backend/django_server/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reviews/', include('reviews.urls')),
-    path('rest-auth/', include('rest-auth.urls')),
+    path('movies/', include('movies.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/signup/', include('rest_auth.registration.urls')),
 ]
