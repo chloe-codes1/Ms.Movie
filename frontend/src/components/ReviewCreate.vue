8a73d69fb97175c17ed848b1fd46653d78ba11d9
오지수 <wltn0213@gmail.com>
2020-06-16 01:50:54 +0900

    Create Review

--- frontend/src/components/ReviewCreate.vue
+++ frontend/src/components/ReviewCreate.vue
@@ -1,29 +1,31 @@
 <template>
-    <div>
+    <div id="create">
         <h3>Create Review</h3>
         <!-- title -->
         <b-form-group class="mb-2" label="Title" label-for="title" description="Write your review for the movie">
-            <b-form-input type="text" id="title" v-model="review.title"> </b-form-input>
+            <b-form-input type="text" id="title" v-model="reviewData.title"> </b-form-input>
         </b-form-group>
         <!-- rating -->
-        <b-form-rating v-model="review.rating" variant="warning" class="mb-2" stars="10"></b-form-rating>
-        <p class="mt-2">Rating: {{ review.rating }}</p>
+        <label for="rating">Rating:</label>
+        <b-form-rating id="rating" inline v-model="reviewData.rating" variant="warning" class="mb-2" stars="10"></b-form-rating>
+        
         <!-- content -->
         <b-form-group class="mb-2" label="Content" label-for="content">
             <b-form-textarea
             id="content"
-            v-model="review.content"
+            v-model="reviewData.content"
             placeholder="Write your review content"
             rows="3"
             max-rows="6"
             ></b-form-textarea>
         </b-form-group>
         
-        <b-button @click="create">Submit</b-button>  
+        <b-button @click="createReview">Submit</b-button>  
     </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
     name: 'ReviewCreate',
     data() {
@@ -36,12 +38,15 @@ export default {
         }
     },
     methods: {
-        create() {this.$emit('createReview', this.reviewData)} 
+        ...mapActions(['createReview'])
     }
     
 }
 </script>
 
 <style>
+#create {
+    width: 50%;
+}
 
 </style>
