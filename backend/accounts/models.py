72bc39a5a078ce5016310111dd6c31a24914a869
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-06-17 02:43:57 +0900

    Add Genre model & custom user model, user profile

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,7 +1,35 @@
 from django.contrib.auth.models import AbstractUser
-from django_mysql.models import Model
+from django.db import models
+from movies.models import Genre, Movie
+import random
+import hashlib
 
 class User(AbstractUser):
-    pass
+    GENRE_CHOICE = [
+        ('Adventure', 'Adventure'),
+        ('Fantasy', 'Fantasy'),
+          ('Animation', 'Animation'),
+          ('Drama', 'Drama'),
+          ('Horror', 'Horror'),
+          ('Action', 'Action'),
+          ('Comedy', 'Comedy'),
+          ('History', 'History'),
+          ('Western', 'Western'),
+          ('Thriller', 'Thriller'),
+          ('Crime', 'Crime'),
+          ('Documentary', 'Documentary'),
+          ('Science Fiction', 'Science Fiction'),
+          ('Mystery', 'Mystery'),
+          ('Music', 'Music'),
+          ('Romance', 'Romance'),
+          ('Family', 'Family'),
+          ('War', 'War'),
+          ('TV Movie', 'TV Movie'),
+        ]
+    favorite = models.CharField(max_length=20, choices=GENRE_CHOICE)
+    @property
+    def gravatar_url(self):
+        return f"https://s.gravatar.com/avatar/{hashlib.md5(self.email.encode('utf-8').strip().lower()).hexdigest()}?s=170&d=mp"
 
-
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
